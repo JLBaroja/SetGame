ca9e0d829f753d14a0bc0c8ecb88f398b5404000
Luis Baroja <j.luis.baroja@gmail.com>
2023-05-16 19:44:23 -0700

    Further plotting functions, ellipse asimmetry

--- generate_cards.R
+++ generate_cards.R
@@ -9,7 +9,7 @@ figure_perimeter <- function(shape, center_x, center_y, width, height){
 	if(shape=='ellipse'){
 		a <- width/2
 		b <- height/2
-		theta <- seq(0,2*pi,length.out=15)
+		theta <- seq(0,2*pi,length.out=10)
 		radius <- (a*b)/(sqrt((b*cos(theta))^2+(a*sin(theta))^2))
 		at_origin <- polar_to_cartesian(radius,theta)
 		x <- at_origin$x+center_x 
@@ -26,6 +26,33 @@ figure_perimeter <- function(shape, center_x, center_y, width, height){
 	return(list(x=x,y=y))
 }
 
+figure_list <- function(n_figures,...){
+	if(n_figures==1){
+		y_centers <- c(0)
+	}
+	if(n_figures==2){
+		y_centers <- c(-.5,.5)
+	}
+	if(n_figures==3){
+		y_centers <- c(-.5,0,.5)
+	}
+	fl <- vector(mode='list',length=n_figures)	
+	for(f in 1:n_figures){
+		fl[[f]] <- figure_perimeter(...,
+				center_x=0,center_y=y_centers[f],
+				width=1,height=0.25)
+	}
+	return(fl)
+}
+
+draw_perimeters <- function(fl){
+	for(f in 1:length(fl)){
+		lines(fl[[f]]$x,fl[[f]]$y,
+		type='o',pch=21,bg='#ffffff',
+		lwd=2,col='#e95698cc')
+	}
+}
+
 blank_polygons <- function(perimenters){
 
 }
@@ -34,10 +61,12 @@ blank_polygons <- function(perimenters){
 # Figure testing:
 try(dev.off())
 x11(width=2.5,height=3.5)
-par(mar=rep(2,4),xaxs='i',yaxs='i')
+par(mar=rep(1,4),xaxs='i',yaxs='i',tck=-0.01,mgp=c(3,0.25,0),
+	cex.axis=0.5)
 plot(NULL,xlim=c(-1,1),ylim=c(-1,1))
-fp <- figure_perimeter('rectangle',center_x=0.25,center_y=-0.5,width=1.5,height=1)
-polygon(fp$x,fp$y,border='red',lwd=2,col='#44558889')	
+draw_perimeters(figure_list(3,shape='ellipse'))
+#fp <- figure_perimeter('rectangle',center_x=0.25,center_y=-0.5,width=1.5,height=1)
+#polygon(fp$x,fp$y,border='red',lwd=2,col='#44558889')	
 
 
 make_card <- function(){
