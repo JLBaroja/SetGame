efae1d48079376a66d84a426eeb53c76ffb94ab7
Luis Baroja <j.luis.baroja@gmail.com>
2024-11-18 00:33:53 -0800

    General backend logic, function that finds all sets in a given board

--- backend_logic.py
+++ backend_logic.py
@@ -1,5 +1,18 @@
 import os
 import random
+import itertools
+
+def is_set(cards):
+	split_cards = [s.split('_') for s in cards]
+	num_unique_values = [len(set([pos[dim] for pos in split_cards])) for dim in range(4)]
+	return all(counts in {1,3} for counts in num_unique_values)
+
+def find_sets(board):
+	combinations = list(itertools.combinations(board,3))
+	are_sets = [is_set(comb) for comb in combinations]
+	sets_indices = [i for i, val in enumerate(are_sets) if val]
+	sets_found = [combinations[i] for i in sets_indices]
+	return sum(are_sets), sets_found
 
 deck_dir = 'Cards/'
 full_deck = os.listdir(deck_dir)
@@ -13,7 +26,9 @@ select = input('Pick cards:')
 int_sel = [int(x) for x in select.split()]
 selected_cards = [board[i] for i in int_sel]
 
-def is_set(cards):
-	split_cards = [s.split('_') for s in cards]
-	num_unique_values = [len(set([pos[dim] for pos in split_cards])) for dim in range(4)]
-	return all(counts in {1,3} for counts in num_unique_values)
+print(is_set(selected_cards))
+print(find_sets(board))
+
+
+
+
